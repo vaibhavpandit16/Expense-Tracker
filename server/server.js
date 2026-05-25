43f563c8d061eaee4aa6a754c5af9e71c8c69d74
Vaibhav Pandit <vaibhavpandit1612@gmail.com>
2026-05-25 16:06:19 +0530

    fixed server deployment

--- server/server.js
+++ server/server.js
@@ -4,49 +4,41 @@ const cors = require("cors");
 require("dotenv").config();
 
 const expenseRoutes = require("./routes/expenseRoutes");
-
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 
-// IMPORTANT
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
 
 // ROUTES
 app.use("/api/expenses", expenseRoutes);
-
 app.use("/api/auth", authRoutes);
 
 
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Expense Tracker API Running");
 });
 
 
-const PORT =
-  process.env.PORT || 5000;
+// CONNECT DATABASE
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
 
-app.listen(PORT, () => {
+    console.log("MongoDB Connected");
 
-  console.log(
-    `Server running on port ${PORT}`
-  );
+    const PORT = process.env.PORT || 5000;
 
-});
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-
-  console.log("MongoDB Connected");
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  })
+  .catch((error) => {
+    console.log(error);
   });
-
-})
-.catch((error) => {
-  console.log(error);
-});
