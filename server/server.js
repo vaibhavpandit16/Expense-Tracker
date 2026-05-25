63e85086d9058e6f3acb84ae05fa4f67004de97e
Vaibhav Pandit <vaibhavpandit1612@gmail.com>
2026-05-25 15:24:52 +0530

    Fixed Render deployment

--- server/server.js
+++ server/server.js
@@ -26,7 +26,16 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
+});
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
